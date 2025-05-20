df1c92c96d0c0d077ad53ff0ab8bc7b8c54eec05
Syalaisha-Nisrina <ichanisrina04@gmail.com>
2025-05-20 11:35:24 +0700

    hapus

--- template-pbo/src/main/java/com/kidaro/kael/repository/TransactionItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kidaro.kael.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kidaro.kael.model.TransactionItem;
-
-public interface TransactionItemRepository extends JpaRepository<TransactionItem, Long> {}
